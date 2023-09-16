ed1bf7a41eaa2fc55d684e7f19d4d8fda22f5c7a
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-09-16 17:58:33 +0800

    Final Commit

--- portfol/urls.py
+++ portfol/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.home,name='portfolio-home'),
     path('main/', views.main,name='portfolio-main'),
     path('about/', views.about,name='portfolio-about'),
+    path('contact/', views.contact,name='portfolio-contact'),
+
 ]
--- portfol/views.py
+++ portfol/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 def home(request):
     return render(request, 'portfol/home.html')
@@ -8,3 +9,16 @@ def main(request):
 
 def about(request):
     return render(request,'portfol/about.html')
+
+def contact(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.save()
+    return render(request,'portfol/contact.html')
+
